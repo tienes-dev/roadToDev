000082951e85fb21a1e1d982b84565824ea7d99b
nguyenTien <tienes.nguyen@dev.com>
2019-10-20 23:09:19 +0700

    add Cat object

---
+++ cat.js
@@ -0,0 +1,11 @@
+
+function Cat(name, weight, age) {
+	this.name = name;
+	this.weight = weight;
+	this.age = age;
+}
+
+jerry.prototype.eat = function(rice) {
+	this.stomach = [];
+	stomach.push(rice);
+};
